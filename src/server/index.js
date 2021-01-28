4bba1c8a16574515714d12ec906051087fb9fd29
Moses Kamara <kamara.moses@yahoo.com>
2021-01-27 19:00:44 -0800

    made some minor changes to the express server

--- src/server/index.js
+++ src/server/index.js
@@ -1,26 +1,28 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+var path = require("path")
+const express = require("express")
+const mockAPIResponse = require("./mockAPI.js")
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config()
 
 const app = express()
 
-app.use(express.static('dist'))
+app.use(cors());
+app.use(express.static("dist"))
 
 console.log(__dirname)
 
-// call to the meanincloud API
-const baseUrl = "https://api.meaningcloud.com/sentiment-2.1?"
-
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('dist/index.html'))
+app.get("/", function (req, res) {
+    // res.sendFile("dist/index.html")
+    res.sendFile(path.resolve("dist/index.html"))
 })
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
+    console.log("Example app listening on port 8081!")
 })
 
-app.get('/test', function (req, res) {
+app.get("/test", function (req, res) {
     res.send(mockAPIResponse)
 })
+
