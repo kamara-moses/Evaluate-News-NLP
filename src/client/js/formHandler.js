0b4ba4c12f5d10f6a74fb9074fb42298d7aa8f6e
Moses Kamara <kamara.moses@yahoo.com>
2021-01-28 20:14:35 -0800

    created the score criteria for the response to check against

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -63,31 +63,31 @@ const getKey = async () => {
 
 export { getKey }
 
-// const polarityGet = async (polarity) => {
-//     let text = ''
-//     switch (polarity) {
-//         case 'P+':
-//             text = 'Strongly Positive'
-//             break;
-//         case 'P':
-//             text = 'Fairly Positive'
-//             break;
-//         case 'NEU':
-//             text = 'Neutral'
-//             break;
-//         case 'N':
-//             text = 'Negative'
-//             break;
-//         case 'N+':
-//             text = 'Strongly Negative'
-//             break;
-//         case 'NONE':
-//             text = 'Without Sentiment'
-//             break;
-//         default:
-//             text = "No sentiment found..."
-//     }
-//     return text;
-// }
+const polarityGet = async (polarity) => {
+    let text = ''
+    switch (polarity) {
+        case 'P+':
+            text = 'Strongly Positive'
+            break;
+        case 'P':
+            text = 'Fairly Positive'
+            break;
+        case 'NEU':
+            text = 'Neutral'
+            break;
+        case 'N':
+            text = 'Negative'
+            break;
+        case 'N+':
+            text = 'Strongly Negative'
+            break;
+        case 'NONE':
+            text = 'Without Sentiment'
+            break;
+        default:
+            text = "No sentiment found..."
+    }
+    return text;
+}
 
-// export { polarityGet }
+export { polarityGet }
