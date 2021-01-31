6ca4f2530def1a482f5dc75c952d8bcc19d720ff
Moses Kamara <kamara.moses@yahoo.com>
2021-01-30 23:03:03 -0800

    made minor changes to the server

--- src/server/index.js
+++ src/server/index.js
@@ -16,7 +16,7 @@ app.use(express.static("dist"))
 console.log(__dirname)
 
 
-app.get("*", function (req, res) {
+app.get("/", function (req, res) {
     // res.sendFile("dist/index.html")
     res.sendFile(path.resolve("dist/index.html"))
 })
