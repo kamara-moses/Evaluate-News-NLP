39c6a53c1f6c21537ca3315e99b79a494b064cc5
Moses Kamara <kamara.moses@yahoo.com>
2021-01-30 21:58:44 -0800

    deploy

--- src/server/index.js
+++ src/server/index.js
@@ -23,9 +23,10 @@ app.get("/", function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8081, function () {
-    console.log("Example app listening on port 8081!")
-})
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
 
 // app.get("/test", function (req, res) {
 //     res.send(mockAPIResponse)
